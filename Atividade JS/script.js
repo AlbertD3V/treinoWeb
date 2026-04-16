811606515b357857e11cc725b90ec7a696e2b14b
AlbertCarneiro <albrtcd@gmail.com>
2026-04-16 12:36:33 -0300

    update

---
+++ Atividade JS/script.js
@@ -0,0 +1,12 @@
+class Produto{
+    constructor (Codigo, Name, Qtd,SinglePrice,TotalPrice,TotalBuy){
+        this.Codigo = Codigo;
+        this.Name = Name;
+        this.Qtd = Qtd;
+        this.SinglePrice = SinglePrice;
+        this.TotalPrice = TotalPrice;
+        this.TotalBuy = TotalBuy;
+        
+
+    }
+}
